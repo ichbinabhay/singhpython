e553f31b32fb53c0d2c7f6cac1e79193030bd0b9
Abhay singh <ichbinabhaysingh1111@gmail.com>
2026-09-17 19:38:32 +0530

    added sum2.py

---
+++ sum2.py
@@ -0,0 +1,6 @@
+sum=0
+for n in range(1,8):
+    sum+=n
+print("sum of natural numbers <=",n,'is',sum)    
+#note that in this program print is written in starting line so it is giving us direct sum of 7 natural numbers, but if we used print just below sum then it will print all subsequent nnatural number till 7 and sum them like we done in for 2.py
+ 
